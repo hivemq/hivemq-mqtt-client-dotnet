d082230da3d90f92520bc8869c86a7568929dc90
Peter Giacomo Lombardo <pglombardo@hey.com>
2023-12-19 11:29:49 +0100

    Fix tests

--- Tests/HiveMQtt.Test/HiveMQClient/SubscribeBuilderTest.cs
+++ Tests/HiveMQtt.Test/HiveMQClient/SubscribeBuilderTest.cs
@@ -24,8 +24,8 @@ public class SubscribeBuilderTest
         var subResult = await subClient.SubscribeAsync(subscribeOptions).ConfigureAwait(false);
 
         Assert.NotEmpty(subResult.Subscriptions);
-        Assert.Equal(SubAckReasonCode.GrantedQoS0, subResult.Subscriptions[0].SubscribeReasonCode);
-        Assert.Equal(SubAckReasonCode.GrantedQoS0, subResult.Subscriptions[1].SubscribeReasonCode);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[0].SubscribeReasonCode);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[1].SubscribeReasonCode);
 
         var disconnectResult = await subClient.DisconnectAsync().ConfigureAwait(false);
         Assert.True(disconnectResult);
@@ -47,11 +47,11 @@ public class SubscribeBuilderTest
         var subResult = await subClient.SubscribeAsync(subscribeOptions).ConfigureAwait(false);
 
         Assert.NotEmpty(subResult.Subscriptions);
-        Assert.Equal(SubAckReasonCode.GrantedQoS0, subResult.Subscriptions[0].SubscribeReasonCode);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[0].SubscribeReasonCode);
         Assert.Equal(true, subResult.Subscriptions[0].TopicFilter.NoLocal);
         Assert.Equal(true, subResult.Subscriptions[0].TopicFilter.RetainAsPublished);
         Assert.Equal(MQTT5.Types.RetainHandling.SendAtSubscribe, subResult.Subscriptions[0].TopicFilter.RetainHandling);
-        Assert.Equal(SubAckReasonCode.GrantedQoS0, subResult.Subscriptions[1].SubscribeReasonCode);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[1].SubscribeReasonCode);
 
         var disconnectResult = await subClient.DisconnectAsync().ConfigureAwait(false);
         Assert.True(disconnectResult);
