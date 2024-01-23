a0ddc82a601acf0f0b16941b09a1b8acddecacb6
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-01-23 12:12:44 +0100

    Add test showing dual message handlers

--- Tests/HiveMQtt.Test/HiveMQClient/SubscribeBuilderTest.cs
+++ Tests/HiveMQtt.Test/HiveMQClient/SubscribeBuilderTest.cs
@@ -2,6 +2,7 @@ namespace HiveMQtt.Test.HiveMQClient;
 
 using System.Threading.Tasks;
 using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
 using HiveMQtt.MQTT5.ReasonCodes;
 using Xunit;
 
@@ -87,6 +88,68 @@ public class SubscribeBuilderTest
 
         var handlerResult = await tcs.Task.WaitAsync(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
         Assert.True(handlerResult);
+
+        var disconnectResult = await subClient.DisconnectAsync().ConfigureAwait(false);
+        Assert.True(disconnectResult);
+    }
+
+    [Fact]
+    public async Task PerSubscriptionHandlerAndGlobalHandlerAsync()
+    {
+        var subClient = new HiveMQClient();
+        var connectResult = await subClient.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        var tcs = new TaskCompletionSource<bool>();
+        var gtcs = new TaskCompletionSource<bool>();
+
+        // Setup a per-subscription handler
+        var subscribeOptions = new SubscribeOptionsBuilder()
+            .WithSubscription("tests/PerSubscriptionHandler", MQTT5.Types.QualityOfService.AtLeastOnceDelivery, messageReceivedHandler: (sender, args) =>
+            {
+                Assert.Equal("tests/PerSubscriptionHandler", args.PublishMessage.Topic);
+                Assert.Equal("test", args.PublishMessage.PayloadAsString);
+                tcs.SetResult(true);
+            })
+            .Build();
+
+        // Setup a global message handler
+        void GlobalMessageHandler(object? sender, OnMessageReceivedEventArgs eventArgs)
+        {
+            Assert.Equal("tests/PerSubscriptionHandler", eventArgs.PublishMessage.Topic);
+            Assert.Equal("test", eventArgs.PublishMessage.PayloadAsString);
+            gtcs.SetResult(true);
+        }
+
+        subClient.OnMessageReceived += GlobalMessageHandler;
+
+        // Both handlers should be called
+        var subResult = await subClient.SubscribeAsync(subscribeOptions).ConfigureAwait(false);
+
+        Assert.NotEmpty(subResult.Subscriptions);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[0].SubscribeReasonCode);
+
+        var pubClient = new HiveMQClient();
+        var pubConnectResult = await pubClient.ConnectAsync().ConfigureAwait(false);
+        Assert.True(pubConnectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        var pubResult = await pubClient.PublishAsync("tests/PerSubscriptionHandler", "test").ConfigureAwait(false);
+
+        // Wait on both TaskCompletionSource objects - both handlers should get called
+        var timeout = TimeSpan.FromSeconds(10);
+        var delayTask = Task.Delay(timeout);
+        var completedTask = await Task.WhenAny(Task.WhenAll(tcs.Task, gtcs.Task), delayTask).ConfigureAwait(false);
+
+        if (completedTask == delayTask)
+        {
+            throw new TimeoutException("The operation has timed out.");
+        }
+
+        // If we reach here, it means both tasks have completed before the timeout
+        var handlerResult = await tcs.Task.ConfigureAwait(false);
+        var globalHandlerResult = await gtcs.Task.ConfigureAwait(false);
+        Assert.True(handlerResult);
+        Assert.True(globalHandlerResult);
 
         var disconnectResult = await subClient.DisconnectAsync().ConfigureAwait(false);
         Assert.True(disconnectResult);
