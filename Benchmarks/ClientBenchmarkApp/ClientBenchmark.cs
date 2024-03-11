85d26401c819345a935ed380b27aa72c616009d6
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-03-11 16:12:31 +0100

    PublishAsync Benchmarks

--- Benchmarks/ClientBenchmarkApp/ClientBenchmark.cs
+++ Benchmarks/ClientBenchmarkApp/ClientBenchmark.cs
@@ -3,17 +3,24 @@ namespace ClientBenchmarkApp;
 using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Engines;
 
 using HiveMQtt.Client;
 using HiveMQtt.Client.Options;
+using HiveMQtt.MQTT5;
 using HiveMQtt.MQTT5.ReasonCodes;
 using HiveMQtt.MQTT5.Types;
 
-public class ClientBenchmark
+[SimpleJob(RunStrategy.Monitoring, iterationCount: 10, id: "MonitoringJob")]
+public class ClientBenchmarks
 {
     private HiveMQClient client;
 
-    public ClientBenchmark()
+    private string smallPayload = new string(/*lang=json,strict*/ "{\"interference\": \"1029384\"}");
+
+    [GlobalSetup]
+    public void Setup()
     {
         var options = new HiveMQClientOptions
         {
@@ -24,22 +31,53 @@ public class ClientBenchmark
         this.client = new HiveMQClient(options);
         Console.WriteLine($"Connecting to {options.Host} on port {options.Port}...");
         this.client.ConnectAsync().Wait();
-    }
 
-    [Benchmark]
-    public void Publish100QoS0Messages()
-    {
-        // Code to benchmark
-        for (var i = 0; i < 100; i++)
+        if (this.client.IsConnected())
         {
-            // Add your code to benchmark here
-            var result = this.SomeMethodToBenchmark();
+            Console.WriteLine("HiveMQ client connected.");
+        }
+        else
+        {
+            Console.WriteLine("Client failed to connect!");
         }
     }
 
-    private int SomeMethodToBenchmark()
+    [GlobalCleanup]
+    public void CleanUp()
     {
-        // Example method to benchmark
-        return 42;
+        Console.WriteLine("Disconnecting from HiveMQ...");
+        this.client.DisconnectAsync().Wait();
+    }
+
+    // [Benchmark(Description = "Publish 100 QoS 0 messages to the broker.")]
+    // public void Publish100QoS0Messages()
+    // {
+    //     var msg = new string(/*lang=json,strict*/ "{\"interference\": \"1029384\"}");
+    //     for (var i = 0; i < 100; i++)
+    //     {
+    //         this.client.PublishAsync("benchmarks/Publish100QoS0Messages", msg).Wait();
+    //     }
+    // }
+
+    [Benchmark(Description = "Publish a QoS 0 messages to the broker.")]
+    public void PublishQoS0Message()
+    {
+        var task = this.client.PublishAsync("benchmarks/PublishQoS0Messages", this.smallPayload);
+        task.Wait();
+    }
+
+
+    [Benchmark(Description = "Publish a QoS 1 messages to the broker.")]
+    public void PublishQoS1Message()
+    {
+        var task = this.client.PublishAsync("benchmarks/PublishQoS1Messages", this.smallPayload, QualityOfService.AtLeastOnceDelivery);
+        task.Wait();
+    }
+
+    [Benchmark(Description = "Publish a QoS 2 messages to the broker.")]
+    public void PublishQoS2Message()
+    {
+        var task = this.client.PublishAsync("benchmarks/PublishQoS1Messages", this.smallPayload, QualityOfService.ExactlyOnceDelivery);
+        task.Wait();
     }
 }
