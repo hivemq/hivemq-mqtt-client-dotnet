1491d96ef1953134a1b3b1b5f2a9a5692a747ab1
Peter Giacomo Lombardo <pglombardo@hey.com>
2024-05-16 16:28:20 +0200

    Formatting

--- Source/HiveMQtt/Client/HiveMQClientTrafficProcessor.cs
+++ Source/HiveMQtt/Client/HiveMQClientTrafficProcessor.cs
@@ -172,6 +172,7 @@ public partial class HiveMQClient : IDisposable, IHiveMQClient
                     {
                         break;
                     }
+
                 }
                 else
                 {
