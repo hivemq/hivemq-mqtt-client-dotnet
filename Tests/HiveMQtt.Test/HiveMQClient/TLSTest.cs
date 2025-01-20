9f68d67e6196606d4396f8a30b8c94c7ccc01265
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-01-20 11:04:13 +0100

    Disable this test until an alternative is found

--- Tests/HiveMQtt.Test/HiveMQClient/TLSTest.cs
+++ Tests/HiveMQtt.Test/HiveMQClient/TLSTest.cs
@@ -1,31 +1,31 @@
-namespace HiveMQtt.Test.HiveMQClient;
+// namespace HiveMQtt.Test.HiveMQClient;
 
-using System.Threading.Tasks;
-using HiveMQtt.Client;
-using HiveMQtt.Client.Options;
-using HiveMQtt.MQTT5.ReasonCodes;
-using Xunit;
+// using System.Threading.Tasks;
+// using HiveMQtt.Client;
+// using HiveMQtt.Client.Options;
+// using HiveMQtt.MQTT5.ReasonCodes;
+// using Xunit;
 
-public class TLSTest
-{
-#pragma warning disable xUnit1004
-    [Fact(Skip = "Github CI failing: Need to be run manually")]
-#pragma warning restore xUnit1004
-    public async Task Public_Broker_TLS_Async()
-    {
-        var options = new HiveMQClientOptions
-        {
-            Host = "broker.hivemq.com",
-            Port = 8883,
-        };
+// public class TLSTest
+// {
+// #pragma warning disable xUnit1004
+//     [Fact(Skip = "Github CI failing: Need to be run manually")]
+// #pragma warning restore xUnit1004
+//     public async Task Public_Broker_TLS_Async()
+//     {
+//         var options = new HiveMQClientOptions
+//         {
+//             Host = "broker.hivemq.com",
+//             Port = 8883,
+//         };
 
-        var client = new HiveMQClient(options);
+//         var client = new HiveMQClient(options);
 
-        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+//         var connectResult = await client.ConnectAsync().ConfigureAwait(false);
 
-        Assert.Equal(ConnAckReasonCode.Success, connectResult.ReasonCode);
+//         Assert.Equal(ConnAckReasonCode.Success, connectResult.ReasonCode);
 
-        var disconnectResult = await client.DisconnectAsync().ConfigureAwait(false);
-        Assert.True(disconnectResult);
-    }
-}
+//         var disconnectResult = await client.DisconnectAsync().ConfigureAwait(false);
+//         Assert.True(disconnectResult);
+//     }
+// }
