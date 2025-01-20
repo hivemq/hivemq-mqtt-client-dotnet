198b9590282386c533a3b3683e0f27676e4c09e1
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-01-20 12:39:37 +0100

    Warnings fix

--- Tests/HiveMQtt.Test/HiveMQClient/TLSTest.cs
+++ Tests/HiveMQtt.Test/HiveMQClient/TLSTest.cs
@@ -1,11 +1,11 @@
 // namespace HiveMQtt.Test.HiveMQClient;
-
+//
 // using System.Threading.Tasks;
 // using HiveMQtt.Client;
 // using HiveMQtt.Client.Options;
 // using HiveMQtt.MQTT5.ReasonCodes;
 // using Xunit;
-
+//
 // public class TLSTest
 // {
 // #pragma warning disable xUnit1004
@@ -18,13 +18,10 @@
 //             Host = "broker.hivemq.com",
 //             Port = 8883,
 //         };
-
+//
 //         var client = new HiveMQClient(options);
-
 //         var connectResult = await client.ConnectAsync().ConfigureAwait(false);
-
 //         Assert.Equal(ConnAckReasonCode.Success, connectResult.ReasonCode);
-
 //         var disconnectResult = await client.DisconnectAsync().ConfigureAwait(false);
 //         Assert.True(disconnectResult);
 //     }
