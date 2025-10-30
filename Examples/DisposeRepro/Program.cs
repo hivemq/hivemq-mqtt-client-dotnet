59efcddb88c47586b27f66347e53d0353ec1981a
Peter Giacomo Lombardo <pglombardo@hey.com>
2025-10-30 15:56:24 +0100

    Add new Disposal Example App

---
+++ Examples/DisposeRepro/Program.cs
@@ -0,0 +1,30 @@
+﻿using HiveMQtt.Client;
+using HiveMQtt.Client.Options;
+
+Console.CancelKeyPress += (_, e) =>
+{
+    Console.WriteLine("CTRL+C pressed.");
+    e.Cancel = false;
+};
+
+var options = new HiveMQClientOptions
+{
+    Host = "broker.hivemq.com",
+    Port = 1883,
+    ClientId = $"dispose-repro-{Guid.NewGuid():N}"
+};
+
+using var client = new HiveMQClient(options);
+
+Console.WriteLine("Connecting...");
+await client.ConnectAsync();
+
+Console.WriteLine("Disconnecting...");
+await client.DisconnectAsync();
+
+Console.WriteLine("Disposing...");
+client.Dispose();
+
+Console.WriteLine("Disposed.");
+
+await Task.Delay(1000);
