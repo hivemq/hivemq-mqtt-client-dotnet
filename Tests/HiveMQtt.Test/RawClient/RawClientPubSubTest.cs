710d72b1864e62bee071fa27e1d03a640756b301
Peter Giacomo Lombardo <pglombardo@hey.com>
2026-01-11 23:48:25 +0100

    More tests

---
+++ Tests/HiveMQtt.Test/RawClient/RawClientPubSubTest.cs
@@ -0,0 +1,183 @@
+namespace HiveMQtt.Test.RawClient;
+
+using System.Threading.Tasks;
+using HiveMQtt.Client;
+using HiveMQtt.MQTT5.ReasonCodes;
+using HiveMQtt.MQTT5.Types;
+using Xunit;
+
+public class RawClientPubSubTest
+{
+    [Fact]
+    public async Task MostBasicPubSubAsync()
+    {
+        var testTopic = "tests/RawClientMostBasicPubSubAsync";
+        var testPayload = new string(/*lang=json,strict*/ "{\"interference\": \"1029384\"}");
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientMostBasicPubSubAsync").Build();
+        var client = new RawClient(options);
+        var taskCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        // Set the event handler for the message received event
+        client.OnMessageReceived += (sender, args) =>
+        {
+            Assert.Equal(testTopic, args.PublishMessage.Topic);
+            Assert.Equal(testPayload, args.PublishMessage.PayloadAsString);
+
+            // Disconnect after receiving the message
+            if (sender != null)
+            {
+                var rawClient = (RawClient)sender;
+
+                var disconnect = Task.Run(async () =>
+                {
+                    var disconnectResult = await rawClient.DisconnectAsync().ConfigureAwait(false);
+                    Assert.True(disconnectResult);
+                    return disconnectResult;
+                });
+                Assert.True(disconnect.Result);
+            }
+
+            taskCompletionSource.SetResult(true);
+        };
+
+        var subResult = await client.SubscribeAsync(testTopic).ConfigureAwait(false);
+        var result = await client.PublishAsync(testTopic, testPayload).ConfigureAwait(false);
+
+        var taskResult = await taskCompletionSource.Task.WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        Assert.True(taskResult);
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task QoS1PubSubAsync()
+    {
+        var testTopic = "tests/RawClientQoS1PubSubAsync";
+        var testPayload = new string(/*lang=json,strict*/ "{\"interference\": \"1029384\"}");
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientQoS1PubSubAsync").Build();
+        var client = new RawClient(options);
+        var taskCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var messagesReceived = 0;
+
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        // Subscribe with QoS1
+        var subResult = await client.SubscribeAsync(testTopic, QualityOfService.AtLeastOnceDelivery).ConfigureAwait(false);
+        Assert.NotEmpty(subResult.Subscriptions);
+        Assert.Equal(SubAckReasonCode.GrantedQoS1, subResult.Subscriptions[0].SubscribeReasonCode);
+
+        // Set the event handler for the message received event
+        client.OnMessageReceived += (sender, args) =>
+        {
+            Assert.Equal(QualityOfService.AtLeastOnceDelivery, args.PublishMessage.QoS);
+            Assert.Equal(testTopic, args.PublishMessage.Topic);
+            Assert.Equal(testPayload, args.PublishMessage.PayloadAsString);
+
+            Interlocked.Increment(ref messagesReceived);
+            if (messagesReceived == 10 && !taskCompletionSource.Task.IsCompleted)
+            {
+                taskCompletionSource.SetResult(true);
+            }
+        };
+
+        Client.Results.PublishResult result;
+
+        // Publish 10 messages
+        for (var i = 0; i < 10; i++)
+        {
+            result = await client.PublishAsync(testTopic, testPayload, QualityOfService.AtLeastOnceDelivery).ConfigureAwait(false);
+            Assert.IsType<Client.Results.PublishResult>(result);
+            Assert.NotNull(result.QoS1ReasonCode);
+            Assert.Equal(PubAckReasonCode.Success, result?.QoS1ReasonCode);
+        }
+
+        var taskResult = await taskCompletionSource.Task.WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        Assert.True(taskResult);
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task QoS2PubSubAsync()
+    {
+        var testTopic = "tests/RawClientQoS2PubSubAsync";
+        var testPayload = new string(/*lang=json,strict*/ "{\"interference\": \"1029384\"}");
+
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientQoS2PubSubAsync").Build();
+        var client = new RawClient(options);
+        var taskCompletionSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+        var messagesReceived = 0;
+
+        // Subscribe with QoS2
+        var subResult = await client.SubscribeAsync(testTopic, QualityOfService.ExactlyOnceDelivery).ConfigureAwait(false);
+        Assert.NotEmpty(subResult.Subscriptions);
+        Assert.Equal(SubAckReasonCode.GrantedQoS2, subResult.Subscriptions[0].SubscribeReasonCode);
+
+        // Set the event handler for the message received event
+        client.OnMessageReceived += (sender, args) =>
+        {
+            Interlocked.Increment(ref messagesReceived);
+            Assert.Equal(QualityOfService.ExactlyOnceDelivery, args.PublishMessage.QoS);
+            Assert.Equal(testTopic, args.PublishMessage.Topic);
+            Assert.Equal(testPayload, args.PublishMessage.PayloadAsString);
+
+            Assert.NotNull(sender);
+
+            if (messagesReceived >= 5)
+            {
+                taskCompletionSource.SetResult(true);
+            }
+        };
+
+        Client.Results.PublishResult result;
+
+        // Publish 5 messages
+        for (var i = 0; i < 5; i++)
+        {
+            result = await client.PublishAsync(testTopic, testPayload, QualityOfService.ExactlyOnceDelivery).ConfigureAwait(false);
+            Assert.IsType<Client.Results.PublishResult>(result);
+            Assert.NotNull(result.QoS2ReasonCode);
+            Assert.Equal(PubRecReasonCode.Success, result?.QoS2ReasonCode);
+        }
+
+        var taskResult = await taskCompletionSource.Task.WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        Assert.True(taskResult);
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task NoSubscriptionTrackingAsync()
+    {
+        // Verify that RawClient does not maintain subscription state
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientNoSubscriptionTrackingAsync").Build();
+        var client = new RawClient(options);
+
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        // Subscribe to a topic
+        var subResult = await client.SubscribeAsync("test/topic", QualityOfService.AtLeastOnceDelivery).ConfigureAwait(false);
+        Assert.NotEmpty(subResult.Subscriptions);
+
+        // Disconnect and reconnect
+        await client.DisconnectAsync().ConfigureAwait(false);
+        connectResult = await client.ConnectAsync().ConfigureAwait(false);
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+
+        // RawClient should not have any subscription tracking
+        // Note: RawClient doesn't expose Subscriptions property, so we can't verify this directly
+        // But we can verify that re-subscribing works without issues
+        var subResult2 = await client.SubscribeAsync("test/topic2", QualityOfService.AtLeastOnceDelivery).ConfigureAwait(false);
+        Assert.NotEmpty(subResult2.Subscriptions);
+
+        await client.DisconnectAsync().ConfigureAwait(false);
+        client.Dispose();
+    }
+}
---
+++ Tests/HiveMQtt.Test/RawClient/RawClientTest.cs
@@ -0,0 +1,237 @@
+namespace HiveMQtt.Test.RawClient;
+
+using System.Threading.Tasks;
+using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Exceptions;
+using HiveMQtt.Client.Options;
+using HiveMQtt.MQTT5.ReasonCodes;
+using Xunit;
+
+public class RawClientTest
+{
+    [Fact]
+    public async Task BasicConnectAndDisconnectAsync()
+    {
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientBasicConnectAndDisconnectAsync").Build();
+        var client = new RawClient(options);
+        Assert.NotNull(client);
+
+        var connectResult = await client.ConnectAsync().ConfigureAwait(false);
+
+        Assert.True(connectResult.ReasonCode == ConnAckReasonCode.Success);
+        Assert.True(client.IsConnected());
+
+        var disconnectOptions = new DisconnectOptions();
+        Assert.Equal(DisconnectReasonCode.NormalDisconnection, disconnectOptions.ReasonCode);
+
+        var disconnectResult = await client.DisconnectAsync(disconnectOptions).ConfigureAwait(false);
+        Assert.True(disconnectResult);
+        Assert.False(client.IsConnected());
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task RaiseOnFailureToConnectAsync()
+    {
+        // Bad port number
+        var clientOptions = new HiveMQClientOptionsBuilder().WithPort(0).WithClientId("RawClientRaiseOnFailureToConnectAsync").Build();
+        var client = new RawClient(clientOptions);
+
+        await Assert.ThrowsAsync<HiveMQttClientException>(() => client.ConnectAsync()).ConfigureAwait(false);
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task TestConnectEventsAsync()
+    {
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientTestConnectEventsAsync").Build();
+        var client = new RawClient(options);
+
+        // Client Events
+        client.BeforeConnect += BeforeConnectHandler;
+        client.AfterConnect += AfterConnectHandler;
+        client.BeforeDisconnect += BeforeDisconnectHandler;
+        client.AfterDisconnect += AfterDisconnectHandler;
+
+        // Packet Events
+        client.OnConnectSent += OnConnectSentHandler;
+        client.OnConnAckReceived += OnConnAckReceivedHandler;
+
+        // Set up TaskCompletionSource to wait for event handlers to finish
+        var disconnectSentSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var afterDisconnectSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        client.OnDisconnectSent += (sender, args) => disconnectSentSource.TrySetResult(true);
+        client.AfterDisconnect += (sender, args) => afterDisconnectSource.TrySetResult(true);
+
+        // Connect and Disconnect
+        var result = await client.ConnectAsync().ConfigureAwait(false);
+        await client.DisconnectAsync().ConfigureAwait(false);
+
+        // Wait for both disconnect events to complete instead of fixed delay
+        await disconnectSentSource.Task.WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        await afterDisconnectSource.Task.WaitAsync(TimeSpan.FromSeconds(6)).ConfigureAwait(false);
+
+        // Small delay to allow async event handlers to complete (they run via Task.Run)
+        await Task.Delay(100).ConfigureAwait(false);
+
+        // Assert that all Events were called
+        Assert.True(client.LocalStore.ContainsKey("BeforeConnectHandlerCalled"));
+        Assert.True(client.LocalStore.ContainsKey("AfterConnectHandlerCalled"));
+
+        Assert.True(client.LocalStore.ContainsKey("BeforeDisconnectHandlerCalled"));
+        Assert.True(client.LocalStore.ContainsKey("AfterDisconnectHandlerCalled"));
+
+        Assert.True(client.LocalStore.ContainsKey("OnConnectSentHandlerCalled"));
+        Assert.True(client.LocalStore.ContainsKey("OnConnAckReceivedHandlerCalled"));
+
+        // Remove event handlers
+        client.BeforeConnect -= BeforeConnectHandler;
+        client.AfterConnect -= AfterConnectHandler;
+        client.BeforeDisconnect -= BeforeDisconnectHandler;
+        client.AfterDisconnect -= AfterDisconnectHandler;
+
+        client.OnConnectSent -= OnConnectSentHandler;
+        client.OnConnAckReceived -= OnConnAckReceivedHandler;
+
+        client.Dispose();
+    }
+
+    [Fact]
+    public async Task Test_AfterDisconnectEvent_Async()
+    {
+        var options = new HiveMQClientOptionsBuilder().WithClientId("RawClientTest_AfterDisconnectEvent_Async").Build();
+        var client = new RawClient(options);
+
+        // Client Events
+        client.AfterDisconnect += AfterDisconnectHandler;
+
+        // Set up TaskCompletionSource to wait for event handlers to finish
+        var disconnectSentSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var afterDisconnectSource = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        client.OnDisconnectSent += (sender, args) => disconnectSentSource.TrySetResult(true);
+        client.AfterDisconnect += (sender, args) => afterDisconnectSource.TrySetResult(true);
+
+        // Connect and Disconnect
+        var result = await client.ConnectAsync().ConfigureAwait(false);
+        await client.DisconnectAsync().ConfigureAwait(false);
+
+        // Wait for both disconnect events to complete instead of fixed delay
+        await disconnectSentSource.Task.WaitAsync(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+        await afterDisconnectSource.Task.WaitAsync(TimeSpan.FromSeconds(6)).ConfigureAwait(false);
+
+        // Small delay to allow async event handlers to complete (they run via Task.Run)
+        await Task.Delay(100).ConfigureAwait(false);
+
+        // Assert that all Events were called
+        Assert.True(client.LocalStore.ContainsKey("AfterDisconnectHandlerCalled"));
+        Assert.True(client.LocalStore.ContainsKey("AfterDisconnectHandlerCalledCount"));
+        Assert.Equal("1", client.LocalStore["AfterDisconnectHandlerCalledCount"]);
+
+        // Remove event handlers
+        client.AfterDisconnect -= AfterDisconnectHandler;
+
+        client.Dispose();
+    }
+
+    private static void BeforeConnectHandler(object? sender, BeforeConnectEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+            lock (client.LocalStore)
+            {
+                client.LocalStore["BeforeConnectHandlerCalled"] = "true";
+            }
+        }
+
+        Assert.NotNull(eventArgs.Options);
+    }
+
+    private static void OnConnectSentHandler(object? sender, OnConnectSentEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+            lock (client.LocalStore)
+            {
+                client.LocalStore["OnConnectSentHandlerCalled"] = "true";
+            }
+        }
+
+        Assert.NotNull(eventArgs.ConnectPacket);
+    }
+
+    private static void OnConnAckReceivedHandler(object? sender, OnConnAckReceivedEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+            lock (client.LocalStore)
+            {
+                client.LocalStore["OnConnAckReceivedHandlerCalled"] = "true";
+            }
+        }
+
+        Assert.NotNull(eventArgs.ConnAckPacket);
+    }
+
+    private static void AfterConnectHandler(object? sender, AfterConnectEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+            lock (client.LocalStore)
+            {
+                client.LocalStore["AfterConnectHandlerCalled"] = "true";
+            }
+        }
+
+        Assert.NotNull(eventArgs.ConnectResult);
+    }
+
+    private static void BeforeDisconnectHandler(object? sender, BeforeDisconnectEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+            lock (client.LocalStore)
+            {
+                client.LocalStore["BeforeDisconnectHandlerCalled"] = "true";
+            }
+        }
+    }
+
+    private static void AfterDisconnectHandler(object? sender, AfterDisconnectEventArgs eventArgs)
+    {
+        Assert.NotNull(sender);
+        if (sender is not null)
+        {
+            var client = (RawClient)sender;
+
+            lock (client.LocalStore)
+            {
+                if (client.LocalStore.TryGetValue("AfterDisconnectHandlerCalled", out var value))
+                {
+                    var count = Convert.ToInt16(value, System.Globalization.CultureInfo.InvariantCulture);
+                    count++;
+                    client.LocalStore["AfterDisconnectHandlerCalledCount"] = count.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    client.LocalStore["AfterDisconnectHandlerCalled"] = "true";
+                    client.LocalStore["AfterDisconnectHandlerCalledCount"] = "1";
+                }
+            }
+        }
+    }
+}
